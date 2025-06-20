b06d8b40fd8c998da684a24d97eccba1ef3979e9
Conor Nash <conor@nbs.consulting>
2025-06-20 15:17:43 +0100

    Working with Emacs 29, version 30 dictation is not working

--- emacs24.mc.el
+++ emacs24.mc.el
@@ -5,83 +5,58 @@
       '(
         backward-sexp
         beginning-of-visual-line
-        comment-dwim
-        copy-to-register
+        conor-helm-kill-ring-regsters
         cua-copy-region
         cua-cut-region
         cua-exchange-point-and-mark
         cua-paste
         cua-paste-pop
         cycle-spacing
-        djcb-duplicate-line
         downcase-region
-        electric-newline-and-maybe-indent
+        end-of-buffer
         end-of-visual-line
         ess-roxy-move-beginning-of-line
-        ess-smart-comma
         forward-sexp
         hungry-delete-backward
         hungry-delete-forward
         indent-for-tab-command
         insert-register
-        kill-sexp
-        kill-visual-line
-        move-text-down
-        move-text-up
+        kill-region
+        markdown-outdent-or-delete
         my/smarter-move-beginning-of-line
         org-beginning-of-line
+        org-cycle
+        org-delete-char
         org-end-of-line
         org-force-self-insert
-        org-metaright
+        org-kill-line
         org-self-insert-command
-        org-transpose-words
-        python-indent-dedent-line
-        restclient-toggle-body-visibility-or-indent
-        snake-to-camel
+        orgtbl-hijacker-command-109
+        orgtbl-self-insert-command
+        origami-backward-fold-same-level
+        origami-forward-fold-same-level
         sp-backward-delete-char
         sp-backward-kill-word
         sp-backward-sexp
         sp-backward-up-sexp
         sp-delete-char
         sp-down-sexp
-        sp-forward-barf-sexp
         sp-forward-sexp
         sp-forward-slurp-sexp
         sp-join-sexp
         sp-kill-hybrid-sexp
         sp-kill-region
         sp-kill-word
-        sp-remove-active-pair-overlay
         sp-splice-sexp
-        sp-split-sexp
         sp-up-sexp
-        sql-end-of-statement
-        tab-to-tab-stop
-        transpose-sexps
-        transpose-words
-        undo-fu-only-undo
         upcase-region
-        vr/replace
         wdired--self-insert
-        wdired-finish-edit
         yaml-electric-backspace
         yaml-electric-dash-and-dot
-        yas-expand)
+        yas-expand
+        ))
 
-      mc/cmds-to-run-once
+(setq mc/cmds-to-run-once
       '(
-        conor-helm-files
-        cua-scroll-up
-        ess-goto-end-of-function-or-para
-        fill-paragraph
-        handle-switch-frame
         helm-M-x
-        lsp-ui-doc--handle-mouse-movement
-        make-frame-command
-        my/hsplit-last-buffer
-        org-cycle
-        other-frame
-        text-scale-decrease
-        text-scale-increase
-        view-emacs-FAQ
         ))
